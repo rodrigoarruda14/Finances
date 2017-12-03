9d4de9ee7e66ea654923596da4f8f26f7f77f9f6
Rodrigo Arruda <rodrigo.arruda14@gmail.com>
2017-12-03 21:40:15 -0200

    algumas mudanças

--- indicadores ativos.R
+++ indicadores ativos.R
@@ -37,10 +37,21 @@ cycles.dates<-c("2015-12-02/2016-08-31")
 risk.dates = c("2017-05-17")
 risk.labels = c("Delação JBS")
 
-chart.TimeSeries(data[,4], main = 'Banco do Brasil', colorset = "darkblue", 
+chart.TimeSeries(data[,4], main = ii, colorset = "darkblue", 
                  period.areas = cycles.dates, period.color = "lightblue",
                  event.lines = risk.dates, event.labels = risk.labels, 
                  event.color = "darkred", lwd = 2,)  
+
+anos <- x %>% mutate(ano=format(date, '%Y')) %>% distinct(.$ano) %>% rename(ano = ".$ano") %>% t() %>% as.vector()
+close  <- x %>% filter(date >= '2017-01-01') %>% .$close
+
+for(j in anos){
+  
+  close  <- x %>% filter(date >= paste0(j,"-01-01")) %>% .$close
+  print(paste0(j,"-01-01"))
+  teste <- close %>% ave( FUN = function(x) c(0, diff(x))) %>% sign() %>% rle() 
+  print(teste$lengths[teste$values==-1])
+ }
 
 }
 
@@ -49,41 +60,58 @@ chart.TimeSeries(data[,4], main = 'Banco do Brasil', colorset = "darkblue",
 
 
 
-  data <- getSymbols(Symbols = 'BVMF:BBAS3',
-                     src = 'google', 
-                     auto.assign = FALSE)
+data <- getSymbols(Symbols = 'BVMF:BOVA11',
+                   src = 'google', 
+                   auto.assign = FALSE)
   
-  colnames(data) <- c("open","high","low","close","volume")  
+colnames(data) <- c("open","high","low","close","volume")  
   
-  x <- data %>% as.data.frame() %>% mutate(date = index(data)) %>% drop_na() 
-  
-x %>% 
-    mutate(ano = format(date,'%Y'), up = c(((.$close[2:length(.$close)]-.$close[1:length(.$close)-1])>0),"NA")) %>% group_by(ano) %>%
-    summarise(Open = first(open), Close = last(close), Min. = round(min(close,na.rm = TRUE),2), Max. = max(close,na.rm = TRUE), number.ups = sum(up=='TRUE'), number.downs = sum(up=='FALSE')) %>%
-    ungroup() %>% left_join(.,mutate(as.data.frame(round(annualReturn(data),3)), ano = format(index(annualReturn(data)), '%Y'))) %>%
-    knitr::kable()
+x <- data %>% as.data.frame() %>% mutate(date = index(data), valuation = ave(.$close, FUN = function(y) c(0, diff(y)))) %>% drop_na() 
 
-tail(ave(x$close, FUN=function(x) c(0, diff(x))))
-sign(tail(ave(x$close, FUN=function(x) c(0, diff(x)))))
-y <- rle(c(1,0,0,0,1,0,0,0,0,0,2,0,0))
-y$lengths[y$values==0]
+tab <- x %>% 
+  mutate(ano = format(date,'%Y'), up = c(((.$close[2:length(.$close)]-.$close[1:length(.$close)-1])>0),"NA")) %>% group_by(ano) %>%
+  summarise(Open = first(open), Close = last(close), Min. = round(min(close,na.rm = TRUE),2), Max. = max(close,na.rm = TRUE), number.ups = sum(up=='TRUE'), number.downs = sum(up=='FALSE')) %>%
+  ungroup() %>% left_join(.,mutate(as.data.frame(round(annualReturn(data),3)), ano = format(index(annualReturn(data)), '%Y'))) %>%
+  knitr::kable()
+
+print(tab)
+
+cycles.dates<-c("2015-12-02/2016-08-31")
+risk.dates = c("2017-05-17")
+risk.labels = c("Delação JBS")
+
+chart.TimeSeries(data[,4], main = ii, colorset = "darkblue", 
+                 period.areas = cycles.dates, period.color = "lightblue",
+                 event.lines = risk.dates, event.labels = risk.labels, 
+                 event.color = "darkred", lwd = 2,)  
+
+historical.min <- x %>% filter(close==min(close))
+historical.max <- x %>% filter(close==max(close))
 
 
+### average number of consecutive devaluations
 
 anos <- x %>% mutate(ano=format(date, '%Y')) %>% distinct(.$ano) %>% rename(ano = ".$ano") %>% t() %>% as.vector()
+close <- x %>% filter(date >= '2017-01-01') %>% .$close
 
-close  <- x %>% filter(date >= '2017-01-01') %>% .$close
-
-teste <- close %>% ave( FUN = function(x) c(0, diff(x))) %>% sign() %>% rle() 
-table(teste$lengths[teste$values==-1])
-
+downs <- NULL
 
 for(j in anos){
   
-  close  <- x %>% filter(date >= paste0(j,"-01-01")) %>% .$close
-  print(paste0(j,"-01-01"))
-  teste <- close %>% ave( FUN = function(x) c(0, diff(x))) %>% sign() %>% rle() 
-  print(teste$lengths[teste$values==1])
+  base  <- x %>% filter(date >= paste0(j,"-01-01")) 
+  teste <- base$close %>% ave( FUN = function(x) c(0, diff(x))) %>% sign() %>% rle() 
+#  print(j)
+#  print(teste$lengths[teste$values==-1])
+  downs[j] <- mean(teste$lengths[teste$values==-1])
 }
 
 
+
+a <- (teste$lengths[teste$values==-1])
+ks.test(jitter(a),"ppois", lambda = 1.8)
+
+ 
+teste <- x$close %>% ave( FUN = function(x) c(0, diff(x))) %>% sign() %>% rle() 
+a <- teste$lengths[teste$values==-1]
+
+chisq.test(a,rpois(649,2.04))
